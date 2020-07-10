a2f6b6084ecea965e9242a4b30e1d7a5fb67c0fa
George Souza <george_souza97@outlook.com>
2020-07-09 21:07:21 -0300

    Resolução do exercício 1014

---
+++ 1014/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class Main {
+
+	private static String texto = "%s km/l";
+	
+	private static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		BigDecimal distancia = obterNumero();
+		BigDecimal combustivel = obterNumero();
+		exibirResultado(calcularConsumo(distancia, combustivel));
+	}
+	
+	public static BigDecimal obterNumero() {
+		return in.nextBigDecimal();
+	}
+	
+	public static void exibirResultado(BigDecimal consumo) {
+		System.out.println(String.format(texto, consumo.toString()));
+	}
+	
+	public static BigDecimal calcularConsumo(BigDecimal distancia, BigDecimal combustivel) {
+		return distancia.divide(combustivel, 3, RoundingMode.UP);
+	}
+}
